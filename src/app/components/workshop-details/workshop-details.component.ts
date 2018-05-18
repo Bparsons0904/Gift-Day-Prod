2da405d9d3752b574558243b81eec134435fa6e6
Unknown <Deadstylebp@gmail.com>
2018-05-17 22:05:02 -0500

    Workshop V6

--- src/app/components/workshop-details/workshop-details.component.ts
+++ src/app/components/workshop-details/workshop-details.component.ts
@@ -1,4 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+import { WorkshopsService } from '../../services/workshops.service';
+import { Workshop } from '../../models/Workshops';
+import { UserService } from '../../services/user.service';
+import { AuthService } from '../../services/auth.service';
+import { User } from '../../models/User';
+import { Observable } from '@firebase/util';
+import { Presenter } from '../../models/Presenter';
+import { PresenterService } from '../../services/presenter.service';
+
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatFormFieldModule } from '@angular/material';
+import { MatInputModule } from '@angular/material';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule, MatIconRegistry } from '@angular/material/icon';
+import { DomSanitizer } from '@angular/platform-browser';
+import { MatChipsModule } from '@angular/material/chips';
+
+import { MqIfDirective } from '../../directives/mq-if.directive';
+
 
 @Component({
   selector: 'app-workshop-details',
@@ -7,9 +27,168 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkshopDetailsComponent implements OnInit {
 
-  constructor() { }
+  id: string;
+  workshop: Workshop;
+  uid: string;
+  registered: boolean;
+  registered1: boolean;
+  registered2: boolean;
+  registered3: boolean;
+  registeredSession: string;
+  userRegisteredPosition: number;
+  user: User;
+  presenter1: Presenter;
+  presenter2: Presenter;
+  step = 0;
+
+  constructor(
+    private wss: WorkshopsService,
+    private router: Router,
+    private route: ActivatedRoute,
+    private userService: UserService,
+    private auth: AuthService,
+    private presenterService: PresenterService,
+    private matIconRegistry: MatIconRegistry,
+    private domSanitizer: DomSanitizer
+  ) {
+    matIconRegistry.addSvgIconSet(domSanitizer.bypassSecurityTrustResourceUrl('./assets/icons/mdi.svg'));
+   }
 
   ngOnInit() {
+    this.registered = false;
+    // this.uid = this.auth.getAuthID();
+    this.id = this.route.snapshot.params['id'];
+    this.wss.getWorkshop(this.id).subscribe(workshop => {
+      this.workshop = workshop;
+
+      this.auth.user.subscribe(user => {
+        if (user != null) {
+          this.uid = user.uid;
+          this.userService.getUser(this.uid).subscribe(user => {
+            this.user = user;
+            for (let i = 0; i < this.user.workshops.length; i++) {
+              const element = this.user.workshops[i];
+              if (element != null) {
+                this["registered" + (i + 1)] = true;
+              }
+            }
+          });
+          if (this.workshop.session1.registered.indexOf(this.uid) > -1) {
+            this.registered1 = true;
+            this.registered = true;
+            this.registeredSession = '1';
+            // this.registered1Position = this.workshop.session1.registered.indexOf(this.uid)
+            // console.log(this.registered1, this.registered1Position);
+
+          } else {
+            this.registered1 = false;
+          };
+
+          if (this.workshop.session2.registered.indexOf(this.uid) > -1) {
+            this.registered2 = true;
+            this.registered = true;
+            this.registeredSession = '2';
+            // console.log(this.registered1, this.registered1Position);
+
+          } else {
+            this.registered2 = false;
+          };
+          if (this.workshop.session3.registered.indexOf(this.uid) > -1) {
+            this.registered3 = true;
+            this.registered = true;
+            this.registeredSession = '3';
+            // console.log(this.registered1, this.registered1Position);
+
+          } else {
+            this.registered3 = false;
+          };
+        } else {
+          this.uid = null;
+        }
+
+        // if(this.registeredSession != null) {
+        //   this.registered = true;
+        // }
+
+      });
+
+      this.presenterService.getPresenter(this.workshop.presenter1).subscribe(presenter => this.presenter1 = presenter);
+      if (this.workshop.presenter2) {
+        this.presenterService.getPresenter(this.workshop.presenter2).subscribe(presenter => this.presenter2 = presenter);
+      }
+
+
+
+
+      // if(this.uid == undefined) {
+      //   this.auth.user.subscribe(user => {
+      //     this.uid = user.uid;
+      //     console.log(this.uid);
+
+      //     this.userService.getUser(this.uid).subscribe(user => {
+      //       this.user = user;
+      //     });
+
+      //   });
+      // }
+
+      // console.log(this.workshop.registered.indexOf(this.uid), this.uid, this.id);
+
+
+
+    });
+
+    // if(this.workshop.registered.includes(this.id)) {
+    //   console.log("True");
+
+    // } else {
+    //   console.log("False");
+
+    // }
+    // console.log(this.workshop.registered);
   }
 
+  workshopRegister(sessionNumber) {
+    this.workshop['session' + sessionNumber].registered.push(this.uid);
+    // this.workshop.session1.availableSeats -= 1;
+    this.wss.updateWorkshop(this.workshop);
+    this['registered' + sessionNumber] = true;
+    this.registeredSession = sessionNumber;
+    this.registered = true;
+    this["registered" + (sessionNumber + 1)] = true;
+    this.user.workshops.splice((Number(sessionNumber) - 1), 1, this.id);
+    // this.user.workshops.push([this.id, (Number(sessionNumber))]);
+    this.userService.addUserRegistration(this.user);
+  }
+
+  deleteRegistration() {
+    this.workshop['session' + this.registeredSession].registered.splice(this.workshop['session' + this.registeredSession].registered.indexOf(this.uid), 1)
+    // this.workshop['session' + this.registeredSession].availableSeats += 1;
+    this.registered = false;
+    this.registeredSession = null;
+    this.registered1 = false;
+    this.wss.updateWorkshop(this.workshop);
+    this.user.workshops.splice(this.user.workshops.indexOf(this.id), 1, null);
+
+    this.userService.removeUserRegistration(this.user);
+    // console.log(this.workshop['session' + this.registeredSession].registered, this.user.workshops, this.registered);
+  }
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+
+
+  status() {
+    console.log(this.registered);
+
+  }
 }
--- src/app/components/workshops-add/workshops-add.component.ts
+++ src/app/components/workshops-add/workshops-add.component.ts
@@ -1,4 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { FlashMessagesService } from 'angular2-flash-messages';
+import { WorkshopsService } from '../../services/workshops.service';
+import { Workshop } from '../../models/Workshops';
+import { PresenterService } from '../../services/presenter.service';
+import { Presenter } from '../../models/presenter';
+import { AngularFireStorage, AngularFireUploadTask } from 'angularfire2/storage';
+import { Observable } from 'rxjs';
+
+import { AngularFirestore } from 'angularfire2/firestore';
+import { tap } from 'rxjs/operators';
+
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSelectModule } from '@angular/material/select';
 
 @Component({
   selector: 'app-workshops-add',
@@ -7,9 +22,146 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkshopsAddComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+  workshop: Workshop = {
+    name: '',
+    imageURL: '',
+    // imageURL: 'https://placeimg.com/300/240/tech',
+    presenter1: '',
+    presenter2: '',
+    description: '',
+    room: '',
+    session1: {
+      available: false,
+      totalSeats: 0,
+      // availableSeats: 0,
+      registered: [],
+    },
+    session2: {
+      available: false,
+      totalSeats: 0,
+      // availableSeats: 0,
+      registered: [],
+    },
+    session3: {
+      available: false,
+      totalSeats: 0,
+      // availableSeats: 0,
+      registered: [],
+    }
   }
 
+  presenters: Presenter[];
+
+  task: AngularFireUploadTask;
+  percentage: Observable<number>;
+  snapshot: Observable<any>;
+  downloadURL: Observable<string>;
+  isHovering: boolean;
+  imageURL: string;
+
+  @ViewChild('workshopForm') form: any;
+
+  constructor(
+    private flashMessage: FlashMessagesService,
+    private wss: WorkshopsService,
+    private router: Router,
+    private presenterService: PresenterService,
+    private storage: AngularFireStorage,
+  ) { }
+
+  ngOnInit() {
+    this.presenterService.getPresenters().subscribe(presenters => {
+      this.presenters = presenters;
+    });
+  }
+  
+  onSubmit({ value, valid }: { value: Workshop, valid: boolean }) {
+    if (!valid) {
+      this.flashMessage.show('Please fill out the form correctly', {
+        cssClass: 'alert-danger', timeout: 4000
+      });
+    } else {
+      if (this.workshop.session1.totalSeats > 0) {
+        this.workshop.session1.available = true;
+        // this.workshop.session1.availableSeats = this.workshop.session1.totalSeats;
+      };
+      if (this.workshop.session2.totalSeats > 0) {
+        this.workshop.session2.available = true;
+        // this.workshop.session2.availableSeats = this.workshop.session2.totalSeats;
+      };
+      if (this.workshop.session3.totalSeats > 0) {
+        this.workshop.session3.available = true;
+        // this.workshop.session3.availableSeats = this.workshop.session3.totalSeats;
+      };
+      this.wss.newWorkshop(this.workshop);
+      this.flashMessage.show('New workshop added', {
+        cssClass: 'alert-success', timeout: 4000
+      });
+      this.router.navigate(['/workshops']);
+    }
+  }
+
+  toggleHover(event: boolean) {
+    this.isHovering = event;
+  }
+
+
+  startUpload(event: FileList) {
+    // The File object
+    const file = event.item(0)
+
+    // Client-side validation example
+    if (file.type.split('/')[0] !== 'image') {
+      console.error('unsupported file type :( ')
+      return;
+    }
+
+    // The storage path
+    const path = `media/images/workshops/${new Date().getTime()}_${file.name}`;
+
+    // Totally optional metadata
+    const customMetadata = { app: 'GIFT Day App' };
+
+    // The main task
+    this.task = this.storage.upload(path, file, { customMetadata })
+
+    // Progress monitoring
+    this.percentage = this.task.percentageChanges();
+    this.snapshot = this.task.snapshotChanges()
+
+    // this.percentage = this.task.percentageChanges();
+    // this.snapshot   = this.task.snapshotChanges().pipe(
+    //   tap(snap => {
+    //     console.log(snap)
+    //     if (snap.bytesTransferred === snap.totalBytes) {
+    //       this.db.collection('photos').add( { path, size: snap.totalBytes })
+    //     }
+    //   })
+    // )
+
+    // The file's download URL
+    // this.downloadURL = this.task.downloadURL();
+    // console.log(this.task.snapshotChanges());
+
+    this.downloadURL.subscribe(imageURL => {
+      // Path for production
+      // this.imageURL = path;
+
+      // Path for testing
+      this.imageURL = imageURL;
+      this.workshop.imageURL = this.imageURL;
+      // console.log(this.imageURL);
+
+    });
+    // console.log(this.downloadURL);
+
+    // this.workshop.imageURL = String(this.downloadURL);
+  }
+
+
+
+  // Determines if the upload task is active
+  isActive(snapshot) {
+    return snapshot.state === 'running' && snapshot.bytesTransferred < snapshot.totalBytes
+  }
 }
